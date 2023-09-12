35b656b8681ddb1a406fbbd84d5cecf37202f69b
NoahKKA <noahhhhh.a@gmail.com>
2023-09-12 15:07:11 -0700

    added login and logout function also tweaked sign up

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,37 +4,38 @@ import { supabase } from "../supabaseClient";
 import { useState } from "react";
 
 export default function SignUpPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [signUp, setSignUp] = useState(null);
-
-  async function emailSignUpForm() {
-    try {
-      const { user, error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-      });
-
-      if (error) {
-        console.error(
-          `Error signing up with email and password: ${error.message}`
-        );
-        return;
+  const [formData, setFormData] = useState({
+    userName:'',email:'',password:''
+  })
+  console.log(formData)
+  
+  function handleChange(e){
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [e.target.name]:e.target.value
       }
-
-      setSignUp(user);
-    } catch (error) {
-      console.error("An unexpected error occurred:", error.message);
-    }
+    })
   }
 
-  async function googleSignUp() {
-    let { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "google",
-    });
-    setSignUp(data);
-    if (error) {
-      console.log(`Error Signing up with Google, ${error}`);
+  async function handleSubmit(e){
+    e.preventDefault()
+    try {
+      const {data, error} = await supabase.auth.signUp(
+        {
+          email: formData.email,
+          password: formData.password,
+          options: {
+            data: {
+              username: formData.userName
+            }
+          }
+        }
+      )
+      alert('Check your email for verification link')
+
+    } catch(error) {
+      console.log(`Error, ${error}`)
     }
   }
 
@@ -42,33 +43,39 @@ export default function SignUpPage() {
     <Default className="bg-green-100">
       <div>
         <h2>sign up</h2>
-        <form onSubmit={emailSignUpForm} id="signUpForm">
+        <form onSubmit={handleSubmit} id="signUpForm">
           <div>
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="userName" className="text-white">Username: </label>
+            <input
+              type="userName"
+              id="userName"
+              name="userName"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="text-white">Email: </label>
             <input
               type="email"
               id="email"
               name="email"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
+              onChange={handleChange}
               required
             />
           </div>
           <div>
-            <label htmlFor="password">Password:</label>
+            <label htmlFor="password" className="text-white">Password: </label>
             <input
               type="password"
               id="password"
               name="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
+              onChange={handleChange}
               required
             />
           </div>
           <div>
-            <button type="submit">Sign Up</button>
+            <button type="submit" className="text-white">Sign Up</button>
           </div>
         </form>
       </div>
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,4 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 const projectURL= process.env.REACT_APP_DB_URL
 const projectAPIKey = process.env.REACT_APP_DB_API
 
-export const supabase = createClient(projectURL, projectAPIKey)
+export const supabase = createClient(projectURL, projectAPIKey, {
+    autoRefreshToken: true,
+})
