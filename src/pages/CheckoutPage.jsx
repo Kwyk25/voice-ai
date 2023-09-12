0268821bd3816b0f48fad8f773cd99b1bd585a0e
Ryanhu-hub <ryanhuot20@gmail.com>
2023-09-11 19:33:39 -0700

    added checkout page and updated 404 page

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,12 @@
+import "../App.css"
+import Default from "./DefaultPage";
+
+export default function CheckoutPage() {
+    return (
+        <Default className="bg-green-100">
+            <div>
+                <h2>CheckoutPage</h2>
+            </div>
+        </Default>
+    );
+}
--- src/pages/CreditShop.jsx
+++ src/pages/CreditShop.jsx
@@ -1,11 +1,13 @@
 import "../App.css"
 import Default from "./DefaultPage";
+import Button from "react-bootstrap/Button";
 
 export default function CreditShop() {
     return (
         <Default className="bg-green-100">
             <div>
                 <h2>credit</h2>
+                <Button href="/CheckoutPage">Check Out</Button>
             </div>
         </Default>
     );
