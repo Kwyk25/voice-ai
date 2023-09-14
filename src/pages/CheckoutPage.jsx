9c654b26f8c2166ebbc93ddf9d079410b5d200d1
Ryanhu-hub <ryanhuot20@gmail.com>
2023-09-13 18:18:20 -0700

    added styling to checkoutpage

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -6,6 +6,8 @@ export default function CheckoutPage() {
         <Default className="bg-green-100">
             <div>
                 <h2>CheckoutPage</h2>
+                <h1></h1>
+                <p></p>
             </div>
         </Default>
     );
