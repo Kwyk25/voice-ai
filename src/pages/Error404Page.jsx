8b43db35e0b3987d120da496152c689cd1207320
Kyle <kylev006@gmail.com>
2023-09-13 21:12:29 -0700

    cleaned up 404 page

--- src/pages/Error404Page.jsx
+++ src/pages/Error404Page.jsx
@@ -1,16 +1,32 @@
-import "../App.css"
+import "../App.css";
 import Default from "./DefaultPage";
+import error404 from "../assets/images/404.jpg";
 
 export default function Error404Page() {
     return (
-      <Default>
-        <div className="bg-slate-900" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-          <div>
-            <h2 className="error404 text-center">ERROR 404</h2>
-            <h2 className="error404 text-center">Sorry, the page you are trying to reach is unavailable.</h2> <br />
-            <img src="./robot.jpeg" alt="AI VOICE 404" style={{ display: "block", margin: "0 auto" }} />
-          </div>
-        </div>
-      </Default>
+        <Default>
+            <div
+                className="bg-slate-90"
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                }}
+            >
+                <div>
+                    <h1 className="text-white">ERROR 404</h1>
+                    <h2 className="text-white">
+                        Sorry, the page you are trying to reach is unavailable.
+                    </h2>{" "}
+                    <br />
+                   </div>
+                 <img
+                        src={error404}
+                        alt="AI VOICE 404"
+                        style={{ display: "block", margin: "0 auto" }}
+                    />
+            </div>
+        </Default>
     );
-  }
+}
