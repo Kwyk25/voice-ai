52bf749338670f905c231a57e5ce56f66c0a6715
Kyle <kylev006@gmail.com>
2023-09-13 20:07:02 -0700

    styled the login and signup pages

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,78 +2,94 @@ import "../App.css";
 import Default from "./DefaultPage";
 import { supabase } from "../supabaseClient";
 import { useState } from "react";
-import {Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-export default function LoginPage({setToken}) {
-  let navigate = useNavigate()
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-  console.log(formData);
-
-  function handleChange(e) {
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [e.target.name]: e.target.value,
-      };
+export default function LoginPage({ setToken }) {
+    let navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
     });
-  }
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: formData.email,
-        password: formData.password,
-      });
-      
-      if(error) throw error
-      console.log(data)
-      setToken(data)
-      navigate('/')
-    } catch (error) {
-      console.log(`Error, ${error}`);
+    console.log(formData);
+
+    function handleChange(e) {
+        setFormData((prevFormData) => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value,
+            };
+        });
     }
-  }
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email: formData.email,
+                password: formData.password,
+            });
 
-  return (
-    <Default className="bg-green-100">
-      <div>
-        <h2>sign up</h2>
-        <form onSubmit={handleSubmit} id="signUpForm">
-          <div>
-            <label htmlFor="email" className="text-white">
-              Email:{" "}
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="text-white">
-              Password:{" "}
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <button type="submit" className="text-white">
-              Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
-    </Default>
-  );
+            if (error) throw error;
+            console.log(data);
+            setToken(data);
+            navigate("/");
+        } catch (error) {
+            console.log(`Error, ${error}`);
+        }
+    }
+
+    return (
+        <Default>
+            <div className="py-20"></div>
+            <div className="py-20">
+                <Card
+                    className="bg-gradient-to-tr from-pink-500 via-blue-800 to-cyan-500 
+                    mx-auto"
+                    border="dark"
+                    style={{ width: "30rem" }}
+                >
+                    <h2 className="text-white py-4">LOGIN</h2>
+                    <form onSubmit={handleSubmit} id="signUpForm">
+                        <div>
+                            <input
+                                placeholder="example@gmail.com"
+                                className="bg-slate-900 text-white my-2"
+                                type="email"
+                                id="email"
+                                name="email"
+                                onChange={handleChange}
+                                style={{ width: "20rem" }}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <input
+                                placeholder="password"
+                                className="bg-slate-900 text-white my-2"
+                                type="password"
+                                id="password"
+                                name="password"
+                                onChange={handleChange}
+                                style={{ width: "20rem" }}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Button
+                                type="submit"
+                                className="bg-slate-950 text-white my-3"
+                            >
+                                Login
+                            </Button>
+                        </div>
+                    </form>
+                    <p className="text-white">
+                        Don't have an account?{" "}<a href="/SignUpPage" class='underline'>Sign up here.</a>
+                    </p>
+                </Card>
+            </div>
+            <div className="py-12"></div>
+        </Default>
+    );
 }
-
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,86 +3,106 @@ import Button from "react-bootstrap/Button";
 import Default from "./DefaultPage";
 import { supabase } from "../supabaseClient";
 import { useState } from "react";
+import Card from "react-bootstrap/Card";
 
 export default function SignUpPage() {
-  const [formData, setFormData] = useState({
-    userName:'',email:'',password:''
-  })
-  console.log(formData)
-  
-  function handleChange(e){
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [e.target.name]:e.target.value
-      }
-    })
-  }
+    const [formData, setFormData] = useState({
+        userName: "",
+        email: "",
+        password: "",
+    });
+    console.log(formData);
 
-  async function handleSubmit(e){
-    e.preventDefault()
-    try {
-      const {data, error} = await supabase.auth.signUp(
-        {
-          email: formData.email,
-          password: formData.password,
-          options: {
-            data: {
-              username: formData.userName
-            }
-          }
-        }
-      )
-      alert('Check your email for verification link')
-
-    } catch(error) {
-      console.log(`Error, ${error}`)
+    function handleChange(e) {
+        setFormData((prevFormData) => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value,
+            };
+        });
     }
-  }
 
-  return (
-    <Default className="bg-green-100">
-      <div className="pt-5">
-        <h2>sign up</h2>
-        <form onSubmit={handleSubmit} id="signUpForm">
-          <div>
-            <label htmlFor="userName" className="text-white">Username: </label>
-            <input
-              type="userName"
-              id="userName"
-              name="userName"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="text-white">Email: </label>
-            <input
-              className="bg-slate-900 text-white my-2"
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="text-white">Password: </label>
-            <input
-              className="bg-slate-900 text-white my-2"
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <button type="submit" className="text-white">Sign Up</button>
-          </div>
-        </form>
-        If you already have an account click <a href="/LoginPage">Here</a>
-      </div>
-    </Default>
-  );
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const { data, error } = await supabase.auth.signUp({
+                email: formData.email,
+                password: formData.password,
+                options: {
+                    data: {
+                        username: formData.userName,
+                    },
+                },
+            });
+            alert("Check your email for verification link");
+        } catch (error) {
+            console.log(`Error, ${error}`);
+        }
+    }
+
+    return (
+        <Default>
+            <div className="py-20"></div>
+            <div className="py-20">
+                <Card
+                    className="bg-gradient-to-tr from-pink-500 via-blue-800 to-cyan-500 
+                    mx-auto"
+                    border="dark"
+                    style={{ width: "30rem" }}
+                >
+                    <h2 className="text-white py-4">SIGN UP</h2>
+                    <form onSubmit={handleSubmit} id="signUpForm">
+                        <div>
+                            <input
+                                placeholder="username"
+                                className="bg-slate-900 text-white my-2"
+                                type="userName"
+                                id="userName"
+                                name="userName"
+                                onChange={handleChange}
+                                style={{ width: "20rem" }}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <input
+                                placeholder="example@gmail.com"
+                                className="bg-slate-900 text-white my-2"
+                                type="email"
+                                id="email"
+                                name="email"
+                                onChange={handleChange}
+                                style={{ width: "20rem" }}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <input
+                                placeholder="password"
+                                className="bg-slate-900 text-white my-2"
+                                type="password"
+                                id="password"
+                                name="password"
+                                onChange={handleChange}
+                                style={{ width: "20rem" }}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Button
+                                type="submit"
+                                className="bg-slate-950 text-white my-3"
+                            >
+                                Join
+                            </Button>
+                        </div>
+                    </form>
+                    <p className="text-white">
+                    Already have an account?{" "}<a href="/LoginPage" class='underline'>Sign in here.</a>
+                    </p>
+                </Card>
+            </div>
+            <div className="py-12"></div>
+        </Default>
+    );
 }
