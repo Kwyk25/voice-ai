f9cbc93711edd12ae8fb535b55d00cf83cd9604e
Kyle <kylev006@gmail.com>
2023-09-16 12:07:43 -0700

    added the prompt feature

--- src/pages/TtsAiPage.jsx
+++ src/pages/TtsAiPage.jsx
@@ -1,6 +1,8 @@
-import "../App.css"
+import "../App.css";
 import Default from "./DefaultPage";
 import InfoCard from "../components/InfoCard";
+import InputText from "../components/InputText";
+import InputResult from "../components/InputResult";
 
 export default function TtsAiPage() {
     return (
@@ -10,6 +12,14 @@ export default function TtsAiPage() {
                 <div>
                     <h2 className="text-white">How to use the ai voice</h2>
                     <InfoCard />
+                </div>
+                <div className="flex bg-gradient-to-tr from-pink-500 via-blue-800 to-cyan-500 rounded-lg  my-5">
+                    <div className="w-1/2 p-4">
+                        <InputText />
+                    </div>
+                    <div className="w-1/2 p-4">
+                        <InputResult />
+                    </div>
                 </div>
             </div>
         </Default>
