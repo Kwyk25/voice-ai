3ca7b384e3ac0f1b8835e8e477c24d830ae9bc55
NoahKKA <noahhhhh.a@gmail.com>
2023-09-16 12:01:41 -0700

    added username on navbar

---
+++ src/auth/sessionToken.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export default function SessionToken() {
+  const [token, setToken] = useState(false);
+  if (token) {
+    sessionStorage.setItem("token", JSON.stringify(token));
+  }
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      let data = JSON.parse(sessionStorage.getItem("token"));
+      setToken(data);
+    }
+  }, []);
+
+  return token
+}
