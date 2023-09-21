1ec13a22a6dcb5a3cd04e6b7f8ce35e8e506316c
NoahKKA <noahhhhh.a@gmail.com>
2023-09-20 21:11:23 -0700

    fixed audio buttons

--- src/components/ExamplesAi.jsx
+++ src/components/ExamplesAi.jsx
@@ -1,56 +1,61 @@
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-import React from "react";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import { PlayCircleFill } from "react-bootstrap-icons";
+import { PlayCircleFill, PauseCircleFill } from "react-bootstrap-icons";
 import { Link as RouterLink } from "react-router-dom";
 
-let aTest = new Audio(
-    "https://peregrine-results.s3.amazonaws.com/pigeon/wQ6ZI6ZFU9msO8Kn4B_0.wav"
-);
-const pTest = () => {
-    aTest.play();
-};
-
-let aTest2 = new Audio(
-    "https://peregrine-results.s3.amazonaws.com/pigeon/vucxbibY2u74vRnzNn_0.wav"
-);
-const pTest2 = () => {
-    aTest2.play();
-};
-
-let aTest3 = new Audio(
-    "https://peregrine-results.s3.amazonaws.com/pigeon/Qoj88rlnJF2nJ7qzvr_0.wav"
-);
-const pTest3 = () => {
-    aTest3.play();
-};
-
-let aTest4 = new Audio(
-    "https://peregrine-results.s3.amazonaws.com/pigeon/ztigCDV9cHgBxgw5AB_0.wav"
-);
-const pTest4 = () => {
-    aTest4.play();
-};
+const audioData = [
+  {
+    name: "Evan Howard",
+    url: "https://peregrine-results.s3.amazonaws.com/pigeon/wQ6ZI6ZFU9msO8Kn4B_0.wav",
+  },
+  {
+    name: "Elon Musk",
+    url: "https://peregrine-results.s3.amazonaws.com/pigeon/vucxbibY2u74vRnzNn_0.wav",
+  },
+  {
+    name: "Barack Obama",
+    url: "https://peregrine-results.s3.amazonaws.com/pigeon/Qoj88rlnJF2nJ7qzvr_0.wav",
+  },
+  {
+    name: "Donald Trump",
+    url: "https://peregrine-results.s3.amazonaws.com/pigeon/ztigCDV9cHgBxgw5AB_0.wav",
+  },
+];
 
 export default function PlayBtn() {
-    return (
-        <ButtonGroup className="my-4">
-            <Button component={RouterLink} onClick={pTest()}>
-                <PlayCircleFill /> Evan Howard
-            </Button>
-            <Button onClick={pTest2()}>
-                <PlayCircleFill />
-                Elon Musk
-            </Button>
-            <Button onClick={pTest3()}>
-                <PlayCircleFill />
-                Barack Obama
-            </Button>
+  const [audioPlayers, setAudioPlayers] = useState(
+    audioData.map(({ url }) => new Audio(url))
+  );
+  const [playingIndex, setPlayingIndex] = useState(-1);
 
-            <Button onClick={pTest4()}>
-                <PlayCircleFill />
-                Donald Trump
-            </Button>
-        </ButtonGroup>
-    );
+  const toggleAudio = (index) => {
+    if (index === playingIndex) {
+      // Clicked on the same voice, pause it
+      audioPlayers[index].pause();
+      setPlayingIndex(-1);
+    } else {
+      // Clicked on a different voice or for the first time, play it
+      if (playingIndex !== -1) {
+        // Pause the currently playing audio
+        audioPlayers[playingIndex].pause();
+      }
+      audioPlayers[index].play();
+      setPlayingIndex(index);
+    }
+  };
+
+  return (
+    <ButtonGroup className="my-4">
+      {audioData.map(({ name }, index) => (
+        <Button
+          key={index}
+          onClick={() => toggleAudio(index)}
+        >
+          {index === playingIndex ? <PauseCircleFill /> : <PlayCircleFill />}{" "}
+          {name}
+        </Button>
+      ))}
+    </ButtonGroup>
+  );
 }
